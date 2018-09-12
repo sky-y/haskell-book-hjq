f3908af9f003c0c3d01fa179af2c297c8f04530e
Yuki Fujiwara <sky.y.0079@gmail.com>
2018-09-13 00:17:20 +0900

    p.317まで

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-} 
+
+import Test.HUnit
+
+import Data.Hjq.Parser
+
 main :: IO ()
 main = do
-    runTestTT $ TestList
-        [ -- テストケース1
-        , -- テストケース2
-        , -- テストケース3
+    _ <- runTestTT $ TestList
+        [ jqFilterParserTest
         ]
     return ()
+
+jqFilterParserTest :: Test
+jqFilterParserTest = TestList
+    [ "jqFilterParse . " ~: parseJqFilter "." ~?= Right JqNil
+    , "jqFilterParse .[0]" ~: parseJqFilter ".[0]" ~?= Right (JqIndex 0 JqNil)
+    , "jqFilterParse .fieldName" ~: parseJqFilter ".fieldName" ~?= Right (JqField "fieldName" JqNil)
+    , "jqFilterParse .[0].fieldName" ~: parseJqFilter ".[0].fieldName" ~?= Right (JqIndex 0 (JqField "fieldName" JqNil))
+    , "jqFilterParse .fieldName[0]" ~: parseJqFilter ".fieldName[0]" ~?= Right (JqField "fieldName" (JqIndex 0 JqNil))
+    ]
